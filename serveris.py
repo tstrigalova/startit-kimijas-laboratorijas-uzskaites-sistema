19e7b6e9bcb7d420c38a7a36b47fb7b8b172a29c
Tatjana Strigalova <strigalova.tatjana@gmail.com>
2020-12-29 09:23:16 +0000

    pāris routes

--- serveris.py
+++ serveris.py
@@ -7,7 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Labrīt!"
-
+@app.route('/sveiki')
+def sveiki():
+    return "Nav vairs nekāds rīts!"
+@app.route('/sveiki/<vards>')
+def vards():
+    vards = "Tatjana"
+    return "Sveiki, " + vards + "!"
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
