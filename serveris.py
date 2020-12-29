482b4d2ed2736f2947759def1cd4f5d6ae7884fb
Tatjana Strigalova <strigalova.tatjana@gmail.com>
2020-12-29 09:48:23 +0000

    uzdevums1Pabeigts

--- serveris.py
+++ serveris.py
@@ -6,14 +6,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Labrīt!"
+  return "Labrīt!"
 @app.route('/sveiki')
 def sveiki():
-    return "Nav vairs nekāds rīts!"
+  return "Nav vairs nekāds rīts!"
 @app.route('/sveiki/<vards>')
-def vards():
-    vards = "Tatjana"
-    return "Sveiki, " + vards + "!"
+def sveikiPersona(vards):
+  return f"Sveiki {vards}"
+@app.route('/cik/<sk1>/<sk2>')
+def reizinajums(sk1, sk2):
+  return str(int(sk1)*int(sk2))
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
